b169eb4fbb70ad7c55dc3c0964ab8bfe8e9036cd
raseward14 <raseward14@gmail.com>
2025-12-25 09:28:45 -0700

    created custom temperature pipe that transforms temp values by concatenating a degree celsius symbol and letter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-
 import { CommonModule } from '@angular/common';
+
+import { TemperaturePipe } from './temperature.pipe';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [CommonModule],
+  imports: [CommonModule, TemperaturePipe],
 })
 export class AppComponent {
   currentDate = new Date();
