54fb1be2348d6ddf8fa7ce630d883a856194e68f
raseward14 <raseward14@gmail.com>
2025-12-25 09:47:09 -0700

    added conversion equation to transform celcius string to farenheit string

--- src/app/temperature.pipe.ts
+++ src/app/temperature.pipe.ts
@@ -5,7 +5,20 @@ import { Pipe, PipeTransform } from '@angular/core';
     standalone: true
 })
 export class TemperaturePipe implements PipeTransform { 
-  transform(value: any, ...args: any[]) {
-    return value + ' °C';
+  transform(value: string | number) {
+    let val: number;
+
+    if (typeof value === 'string') {
+        // if its a string, convert to number
+        val = parseFloat(value);
+    } else {
+        // else its a number, use it directly
+        val = value;
+    };
+
+    // temp in farenheit = (temp in celsius * 1.8) + 32
+    let outputTemp = (val * 1.8) + 32;
+
+    return `${outputTemp} °F`;
   }  
 }
